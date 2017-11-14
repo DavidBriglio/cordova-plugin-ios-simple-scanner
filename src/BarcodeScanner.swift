e465b7d5443ff7afe900c5ec291627c94143f748
David Briglio <david.briglio@versaterm.com>
2017-11-14 12:52:04 -0500

    Fixed extra bracket

--- src/BarcodeScanner.swift
+++ src/BarcodeScanner.swift
@@ -8,14 +8,13 @@ import UIKit
     
     @objc(capture:)
     func capture(command: CDVInvokedUrlCommand) {
-            let scannerController:BarcodeScannerController = BarcodeScannerController(
-                                                                orientation:command.arguments[0] as? String ?? "",
-                                                                showguide:command.arguments[1] as? String ?? "true",
-                                                                callback:command.callbackId,
-                                                                parent:self)
-            scannerController.modalTransitionStyle = UIModalTransitionStyle.coverVertical;
-            scannerController.modalPresentationStyle = UIModalPresentationStyle.fullScreen;
-            self.viewController?.present(scannerController, animated:true, completion:nil)
-        }
+        let scannerController:BarcodeScannerController = BarcodeScannerController(
+                                                            orientation:command.arguments[0] as? String ?? "",
+                                                            showguide:command.arguments[1] as? String ?? "true",
+                                                            callback:command.callbackId,
+                                                            parent:self)
+        scannerController.modalTransitionStyle = UIModalTransitionStyle.coverVertical;
+        scannerController.modalPresentationStyle = UIModalPresentationStyle.fullScreen;
+        self.viewController?.present(scannerController, animated:true, completion:nil)
     }
 }
